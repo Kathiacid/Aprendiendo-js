6159a109fd9331cc33cb50f894c9bcc3784ffe16
kat <kathiacid98@gmail.com>
2025-06-17 18:35:21 -0400

    d-2

---
+++ D-2/app.js
@@ -0,0 +1,18 @@
+var edadAna,edadMaria,diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2025;
+
+var mayorana = edadAna > edadMaria;
+console.log(mayorana);
+
+var a  =12;
+var b = 5;
+
+var c = a % 5
+console.log(c);
+
+a += b
+console.log(a);
